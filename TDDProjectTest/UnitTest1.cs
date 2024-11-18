d8de8f92b55c6c25c532af1a3666cfbcd26b4bfb
digydigdogdead <carlyle.laurent@gmail.com>
2024-11-18 11:25:16 +0000

    Created test for all Left inputs of the rotate method

--- TDDProjectTest/UnitTest1.cs
+++ TDDProjectTest/UnitTest1.cs
@@ -36,5 +36,23 @@ namespace TDDProjectTest
                 result4.Should().Be(Point.North);
             });
         }
+
+        [TestCase(TestName = "Testing multiple left rotations")]
+        public void MultipleLeftRotateTest()
+        {
+            Compass testCompass = new Compass();
+            var result1 = testCompass.Rotate(Point.North, Direction.Left);
+            var result2 = testCompass.Rotate(Point.East, Direction.Left);
+            var result3 = testCompass.Rotate(Point.South, Direction.Left);
+            var result4 = testCompass.Rotate(Point.West, Direction.Left);
+
+            Assert.Multiple(() =>
+            {
+                result1.Should().Be(Point.West);
+                result2.Should().Be(Point.North);
+                result3.Should().Be(Point.East);
+                result4.Should().Be(Point.South);
+            });
+        }
     }
 }
