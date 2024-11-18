e795021d04b6714354feb302127b5717ea485c9c
digydigdogdead <carlyle.laurent@gmail.com>
2024-11-18 11:23:24 +0000

    Created a MultipleRightRotate test to test all right rotations

--- TDDProjectTest/UnitTest1.cs
+++ TDDProjectTest/UnitTest1.cs
@@ -19,6 +19,22 @@ namespace TDDProjectTest
             result.Should().Be(Point.East);
         }
 
+        [TestCase(TestName = "Testing multiple right rotations")]
+        public void MultipleRightRotateTest()
+        {
+            Compass testCompass = new Compass();
+            var result1 = testCompass.Rotate(Point.North, Direction.Right);
+            var result2 = testCompass.Rotate(Point.East, Direction.Right);
+            var result3 = testCompass.Rotate(Point.South, Direction.Right);
+            var result4 = testCompass.Rotate(Point.West, Direction.Right);
 
+            Assert.Multiple(() =>
+            {
+                result1.Should().Be(Point.East);
+                result2.Should().Be(Point.South);
+                result3.Should().Be(Point.West);
+                result4.Should().Be(Point.North);
+            });
+        }
     }
 }
