b1e45cddb57c79e8885b52b385bd120d4c45342d
digydigdogdead <carlyle.laurent@gmail.com>
2024-11-18 14:53:38 +0000

    Added shopping cart with AddItem and GetTotalPrice functionality and tested both functions

---
+++ TDDProject/ShoppingCart.cs
@@ -0,0 +1,29 @@
+﻿namespace TDDProject
+{
+    public class ShoppingCart
+    {
+        private Dictionary<string, double> items = new Dictionary<string, double>();
+
+        public void AddItem(string item, double price)
+        {
+            if (items.ContainsKey(item))
+            {
+                items[item] += price;
+            }
+            else
+            {
+                items.Add(item, price);
+            }
+        }
+
+        public double GetTotalPrice() 
+        {
+            double sum = 0;
+            foreach (var price in items.Values)
+            {
+                sum += price;
+            }
+            return sum;
+        }
+    }
+}
---
+++ TDDProjectTest/ShoppingCartTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TDDProject;
+using FluentAssertions;
+
+namespace TDDProjectTest
+{
+    internal class ShoppingCartTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void AddItemAndGetTotalPriceTest()
+        {
+            ShoppingCart testCart = new ShoppingCart();
+
+            testCart.AddItem("egg", 20);
+            testCart.AddItem("bread", 10);
+
+            testCart.GetTotalPrice().Should().Be(30);
+        }
+    }
+}
