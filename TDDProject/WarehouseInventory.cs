89402de37637cef161039003ad3b3f6c3a863bf5
Samuel M <76839403+Syntrice@users.noreply.github.com>
2024-11-18 16:58:12 +0000

    Add functionality to WarehoueInventory to add products, get product quantities, and location IDs

--- TDDProject/WarehouseInventory.cs
+++ TDDProject/WarehouseInventory.cs
@@ -8,9 +8,10 @@ namespace TDDProject
 {
     public class WarehouseInventory
     {
+        public const int MaxSize = 64;
+
         public Dictionary<int, Dictionary<int, int>> Locations { get; } = new Dictionary<int, Dictionary<int, int>>();
 
-        public int GetTotalNumberOfProducts() { return 0; }
         public void AddLocation()
         {
             int newLocationId = Locations.Count + 1;
@@ -19,8 +20,73 @@ namespace TDDProject
 
         public void AddProductToLocation(int locationId, int productId, int quantity = 1 )
         {
+            // check if location doesn't exists
 
+            if (!Locations.ContainsKey(locationId))
+            {
+                return;
+            }
+
+            int totalItemCount = GetTotalProductsAtLocation(locationId);
+
+            if ( totalItemCount +  quantity > MaxSize )
+            {
+                return;
+            }
+
+            if (Locations[locationId].ContainsKey(productId))
+            {
+                Locations[locationId][productId] += quantity;
+            } 
+            else
+            {
+                Locations[locationId].Add(productId, quantity);
+            }
         }
 
+        public int GetTotalProductsAtLocation(int locationId)
+        {
+            if (!Locations.ContainsKey(locationId))
+            {
+                return 0;
+            }
+
+            int totalItemCount = 0;
+            foreach (int count in Locations[locationId].Values)
+            {
+                totalItemCount += count;
+            }
+
+            return totalItemCount;
+        }
+
+        public int GetTotalNumberOfProducts()
+        {       
+            int totalItemCount = 0;
+            for (int i = 1; i < Locations.Count + 1; i++)
+            {
+                totalItemCount += GetTotalProductsAtLocation(i);
+            }
+            return totalItemCount;
+        }
+
+        public int GetTotalNumberOfProductsById(int productId)
+        {
+            int totalItemCount = 0;
+            for ( int i = 1; i< Locations.Count + 1; i++)
+            {
+                if ( Locations[i].ContainsKey(productId))
+                {
+                    totalItemCount += Locations[i][productId];
+                }
+            }
+
+            return totalItemCount;
+        }
+
+        public List<int> GetAllLocationIds()
+        {
+            return Locations.Keys.ToList();
+        }
     }
 }
--- TDDProjectTest/WarehouseTests.cs
+++ TDDProjectTest/WarehouseTests.cs
@@ -36,7 +36,79 @@ namespace TDDProjectTest
         }
 
         [Test]
+        public void AddProductToNonexistingLocationTest()
+        {
+            WarehouseInventory testHouse = new WarehouseInventory();
+            testHouse.AddProductToLocation(1, 1);
+            testHouse.Locations.Count.Should().Be(0);
+        }
+
+        [Test]
+        public void AddProductToFullLocationTest()
+        {
+            WarehouseInventory testHouse = new WarehouseInventory();
+            testHouse.AddLocation();
+            testHouse.AddProductToLocation(1, 1, 64);
+            testHouse.Locations[1][1].Should().Be(64);
+            testHouse.AddProductToLocation(1, 1, 3);
+            testHouse.Locations[1][1].Should().Be(64);
+        }
+
+        [Test]
+        public void GetTotalProductsAtLocation()
+        {
+            WarehouseInventory testHouse = new WarehouseInventory();
+            testHouse.GetTotalProductsAtLocation(1).Should().NotBe(5);
+            testHouse.AddLocation();
+            testHouse.AddProductToLocation(1, 1, 5);
+            testHouse.GetTotalProductsAtLocation(1).Should().Be(5);
+
+        }
+
+        [Test]
         public void GetTotalProductsTest()
+        {
+            WarehouseInventory testHouse = new WarehouseInventory();
+            testHouse.GetTotalNumberOfProducts().Should().Be(0);
+            testHouse.AddLocation();
+            testHouse.AddLocation();
+            testHouse.AddProductToLocation(1, 1, 5);
+            testHouse.AddProductToLocation(2, 2, 5);
+            testHouse.GetTotalNumberOfProducts().Should().Be(10);
+        }
+
+        [Test]
+        public void GetTotalProductsByIdTest()
+        {
+            WarehouseInventory testHouse = new WarehouseInventory();
+            testHouse.AddLocation();
+            testHouse.AddLocation();
+            testHouse.AddProductToLocation(1, 1, 5);
+            testHouse.AddProductToLocation(1, 2, 3);
+            testHouse.AddProductToLocation(2, 2, 4);
+            testHouse.GetTotalNumberOfProductsById(1).Should().Be(5);
+            testHouse.GetTotalNumberOfProductsById(2).Should().Be(7);
+        }
+
+        [Test]
+        public void GetAllLocationIdsTest()
+        {
+            WarehouseInventory testHouse = new WarehouseInventory();
+            testHouse.AddLocation();
+            testHouse.AddLocation();
+            testHouse.AddLocation();
+
+            List<int> expectedIds = [ 1, 2, 3 ];
+            List<int> actualIds = testHouse.GetAllLocationIds();
+
+            for (int i = 0; i < expectedIds.Count; i++)
+            {
+                actualIds[i].Should().Be(expectedIds[i]);
+            }
+        }
+
+        [Test]
+        public void GetAllProductIdsTest()
         {
 
         }
